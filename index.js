fdce65bda0c254605a92d3f99a13f5524b9fee21
bgh716 <bgh716@naver.com>
2020-06-20 15:54:54 -0600

    asd

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 var pool;
 pool = new Pool({
-    connectionString:process.env.DATABASE_URL
+    connectionString:'postgres://postgres:SFU716!!qusrlgus@localhost/users'
+    //connectionString:process.env.DATABASE_URL
 })
 
 var app = express();
@@ -30,22 +31,24 @@ app.post('/adduser', (req, res) => {
     var uid = req.body.uid;
     var name = req.body.name;
     var age = req.body.age;
-    var weight = req.body.weight;
+    var size = req.body.size;
     var height = req.body.height;
     var type = req.body.type;
-    var values=[uid, name, age, weight, height, type];
+    var values=[uid, name, age, size, height, type];
     pool.query('SELECT * FROM usr WHERE id=$1', [uid], (error,result)=>{
         if(error)
             res.end(error);
-        else if(result.rows[0] && result.rows[0]['id']==uid){
-            window.alert('id already taken');
+        else if(result&&result.rows[0]){
+            res.send(`USER ID is already taken!`);
         }
         else{
-            var insertUsersQuery=`INSERT INTO usr (id, name, age, weight, height, type) VALUES ($1,$2,$3,$4,$5,$6)`
+            var insertUsersQuery=`INSERT INTO usr (id, name, age, size, height, type) VALUES ($1,$2,$3,$4,$5,$6)`
             pool.query(insertUsersQuery,values, (error,result)=>{
                 if(error)
                     res.end(error);
-                res.send(`USER ID: ${uid} HAS BEEN SUBMITTED!`);
+                else{
+                    res.send(`USER ID: ${uid} HAS BEEN SUBMITTED!`);
+                }
             })
         }
     })
@@ -57,24 +60,38 @@ app.post('/deleteuser', (req, res) => {
     pool.query('DELETE FROM usr WHERE id=$1',[uid], (error,result)=>{
         if(error)
             res.end(error);
-        res.send(`USER ID: ${uid} HAS BEEN DELETED!`);
+        else{
+            res.send(`USER ID: ${uid} HAS BEEN DELETED!`);
+        }
     })
 });
 
 
 app.post('/edituser', (req, res) => {
-    var uid = req.params.uid;
+    var uid = req.body.uid;
     var name = req.body.name;
     var age = req.body.age;
-    var weight = req.body.weight;
+    var size = req.body.size;
     var height = req.body.height;
     var type = req.body.type;
-    var values=[name, age, weight, height, type];
-    var editUsersQuery='UPDATE usr SET name=$1, age=$2, weight=$3, height=$4, type=$5 where id=$6';
-    pool.query(editUsersQuery, values, [uid], (error,result)=>{
+    var values=[name, age, size, height, type, uid];
+    var editUsersQuery='UPDATE usr SET name=$1, age=$2, size=$3, height=$4, type=$5 where id=$6';
+    pool.query(editUsersQuery, values, (error,result)=>{
         if(error)
             res.end(error);
-        res.send(`USER ID: ${uid} HAS BEEN EDITED!`);
+        else{
+            res.send(`USER ID: ${uid} HAS BEEN EDITED!`);
+        }
     })
 });
+
+app.get('/:uid', (req, res) => {
+    var uid = req.params.uid;
+    pool.query('SELECT * FROM usr WHERE id=$1', [uid], (error,result)=>{
+        if(error)
+            res.end(error);
+        res.send(result.rows[0]);
+    })
+});
+
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
